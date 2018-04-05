c1d9e9ba53f10e61a87cc5b2da610cbae8009710
DidierMurillo <didiermurillo@unitec.edu>
2018-04-04 18:30:19 -0600

    JUana

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+tweet = Tweet.create(
+  [
+    {
+      body: "Made of chocolate",
+      username:"Didier"
+
+    },
+    {
+      
+      body: "Only for replying to mentions and DMs",
+      username:"Juana"
+    }
+])
